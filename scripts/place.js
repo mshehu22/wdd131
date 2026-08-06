a33c1e6b26a22e33e5f308895c5f8fce3cf08e85
Magdalene Shehu <lene_06@yahoo.com>
2026-08-06 13:52:10 +0100

    Fix calculateWindChill function to one-line return

--- scripts/place.js
+++ scripts/place.js
@@ -4,23 +4,15 @@ const windSpeed = 10;
 const windChill = document.querySelector("#windchill");
 
 function calculateWindChill(temp, speed) {
-    return (
-        13.12 +
-        0.6215 * temp -
-        11.37 * Math.pow(speed, 0.16) +
-        0.3965 * temp * Math.pow(speed, 0.16)
-    ).toFixed(1);
+    return `${(13.12 + (0.6215 * temp) - (11.37 * Math.pow(speed, 0.16)) + (0.3965 * temp * Math.pow(speed, 0.16))).toFixed(1)} °C`;
 }
 
 if (temperature <= 10 && windSpeed > 4.8) {
-    windChill.textContent =
-        `${calculateWindChill(temperature, windSpeed)} °C`;
+    windChill.textContent = calculateWindChill(temperature, windSpeed);
 } else {
     windChill.textContent = "N/A";
 }
 
-document.querySelector("#currentyear").textContent =
-    new Date().getFullYear();
+document.querySelector("#currentyear").textContent = new Date().getFullYear();
 
-document.querySelector("#lastModified").textContent =
-    `Last Modified: ${document.lastModified}`;
+document.querySelector("#lastModified").textContent = `Last Modified: ${document.lastModified}`;
